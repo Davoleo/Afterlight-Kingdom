1cb4d7ce4477f62325fd5074c4e37d3256fee489
davoleo <dav@davoleo.net>
2026-08-28 17:55:14 +0200

    #47 Level 2 Levers animation and gate open logic

---
+++ Assets/Scripts/Gameplay/Level2GateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Triggers;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class Level2GateController : MonoBehaviour
+    {
+        private List<LeverInteractionHandler> levers;
+
+        private void Start()
+        {
+            levers = FindObjectsByType<LeverInteractionHandler>(FindObjectsSortMode.None).ToList();
+        }
+
+        private void Update()
+        {
+            bool gateOpenReqs = levers.All(lever => lever.WasActivated());
+            Debug.Log( "active levers: "+ levers.Count(lever => lever.WasActivated()) + "/" + levers.Count);
+            if (gateOpenReqs) gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Triggers/LeverInteractionHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace Triggers
+{
+    public class LeverInteractionHandler : MonoBehaviour
+    {
+        private static readonly int FlickedHash = Animator.StringToHash("Flicked");
+
+        private bool _playerInRange;
+        private bool flicked;
+
+        private PlayerCharacterController _player;
+        private Animator _leverAnimator;
+
+        public event Action<bool> LeverStateChanged;
+
+        public bool WasActivated() => flicked;
+
+        private void Start()
+        {
+
+            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacterController>();
+            _leverAnimator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (!_playerInRange) return;
+
+            if (CommandUtils.IsUp(_player.triggers, PlayerTrigger.Interact))
+            {
+                flicked = !flicked;
+                CommandUtils.Off(ref _player.triggers, PlayerTrigger.Interact);
+
+                LeverStateChanged?.Invoke(flicked);
+            }
+
+            _leverAnimator.SetBool(FlickedHash, flicked);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) _playerInRange = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player")) _playerInRange = false;
+        }
+    }
+}
