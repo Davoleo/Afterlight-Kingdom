49852dcbd1faf6367f13766fdffb9fa316dec91c
Gabriele Moisè <g.moise@campus.unimib.it>
2026-08-28 18:39:22 +0200

    Add delay to not show ability widget if no abilities are present.

--- Assets/Scripts/HUD/DashCooldownSource.cs
+++ Assets/Scripts/HUD/DashCooldownSource.cs
@@ -1,4 +1,5 @@
 using Core;
+using Gameplay;
 using Player;
 using UnityEngine;
 
@@ -6,18 +7,23 @@ namespace HUD
 {
     // Adapts PlayerCharacterController's dash Cooldown to IAbilityCooldownSource for
     // AbilityCooldownWidget, so the controller itself doesn't need to know about HUD concerns.
+    // Also hides the overlay entirely while the Dash ability isn't unlocked yet.
     public class DashCooldownSource : MonoBehaviour, IAbilityCooldownSource
     {
         private PlayerCharacterController _controller;
+        private AbilityManager _abilityManager;
 
         private void Start()
         {
             GameObject player = GameObject.FindWithTag("Player");
             _controller = player.GetComponent<PlayerCharacterController>();
+            _abilityManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<AbilityManager>();
         }
 
-        public float Progress01 => _controller.DashCooldown.Progress01;
-        public bool IsReady => _controller.DashCooldown.IsReady;
-        public bool IsEngaged => _controller.DashCooldown.IsEngaged;
+        private bool HasAbility => _abilityManager.HasAbility(AbilityType.Dash);
+
+        public float Progress01 => HasAbility ? _controller.DashCooldown.Progress01 : 0f;
+        public bool IsReady => !HasAbility || _controller.DashCooldown.IsReady;
+        public bool IsEngaged => HasAbility && _controller.DashCooldown.IsEngaged;
     }
 }
