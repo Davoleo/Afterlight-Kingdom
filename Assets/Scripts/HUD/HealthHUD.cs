0a3531a0461e15b9fcdcd0d14ad7629de348e548
davoleo <dav@davoleo.net>
2026-08-30 18:43:12 +0200

    Nerf player health to 4 hearts and remove support for half hearts

--- Assets/Scripts/HUD/HealthHUD.cs
+++ Assets/Scripts/HUD/HealthHUD.cs
@@ -20,23 +20,16 @@ namespace HUD
             _hearts = gameObject.GetComponentsInChildren<Image>();
             
             heartSprites.Add(HeartEnum.Empty, Resources.Load<Sprite>("Sprites/heart_empty"));
-            heartSprites.Add(HeartEnum.Half, Resources.Load<Sprite>("Sprites/heart_half"));
             heartSprites.Add(HeartEnum.Full, Resources.Load<Sprite>("Sprites/heart_full"));
         }
 
         private void Update()
         {
-            int fullHearts = (healthManager.Health / 2);
-            for (int i = 0; i < fullHearts; i++)
+            int heart = 0;
+            while (heart < HealthManager.MaxHealth)
             {
-                _hearts[i].sprite = heartSprites[HeartEnum.Full];
-            }
-
-            if (healthManager.Health < 6)
-            {
-                // odd health value = half-heart | even health value = full heart
-                int lastFullness = healthManager.Health % 2;
-                _hearts[fullHearts].sprite = heartSprites[(HeartEnum) lastFullness];
+                _hearts[heart].sprite = heart < healthManager.Health ? heartSprites[HeartEnum.Full] : heartSprites[HeartEnum.Empty];
+                heart++;
             }
         }
     }
@@ -44,7 +37,6 @@ namespace HUD
     internal enum HeartEnum
     {
         Empty = 0,
-        Half = 1,
         Full = 2
     }
 }
